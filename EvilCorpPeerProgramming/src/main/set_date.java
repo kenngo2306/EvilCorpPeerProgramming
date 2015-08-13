071dbf03509ae591aa1daf4b59b1e2bdfbef6ec0
ahmed <ahha5402@colorado.edu>
2015-08-13 10:34:50 -0400

    Set_date, Transaction , and account are finished .

---
+++ EvilCorpPeerProgramming/src/main/set_date.java
@@ -0,0 +1,31 @@
+package main;
+
+//This class converts date string into integers for Year, Month, and day of week.
+
+public class set_date {
+	private int Day;
+	private int Month;
+	private int Year;
+	
+	
+	public int getDay() {
+		return Day;
+	}
+	
+	public int getMonth() {
+		return Month;
+	}
+	
+	public int getYear() {
+		return Year;
+	}
+	public void setDate(String DATE) {
+		Year = Integer.parseInt(DATE.substring(6, 10));
+		Month = Integer.parseInt(DATE.substring(0, 2)) - 1;
+		Day = Integer.parseInt(DATE.substring(3, 5));
+		
+	
+	}
+	
+	
+}
