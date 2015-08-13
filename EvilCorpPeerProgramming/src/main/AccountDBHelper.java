43689ff3e3b281928bc74d956c930181848369c3
KenNgo <phucuongngo@gmail.com>
2015-08-13 10:43:16 -0400

    resolve conflict

--- EvilCorpPeerProgramming/src/main/AccountDBHelper.java
+++ EvilCorpPeerProgramming/src/main/AccountDBHelper.java
@@ -8,24 +8,52 @@ import java.util.ArrayList;
 
 public class AccountDBHelper
 {
-	public ArrayList<Account> getAllAccounts()
+	public  ArrayList<Account> getAllAccounts()
 	{
 		ArrayList<Account> accounts = new ArrayList<Account>();
+
+        String sql = "select * from accounts";
+
+        //creating PreparedStatement object to execute query
+        ResultSet result = executeSQL(sql);
+        try
+        {
+        	 while(result.next())
+             {
+             	Account account = new Account();
+             	account.setAccount_number(result.getString("account_number"));
+             	account.setID(result.getInt("ID"));
+             	
+             	
+             	
+             	//TODO add fields for accounts
+             	accounts.add(account);
+             }
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        }
+       
 		
 		return accounts;
 	}
 	
-	private Connection getConnection()
+	
+	public ResultSet executeSQL(String sql)
 	{
 		Connection conn = null;
+		ResultSet result = null;
 		try
 		{
 			conn =  DBConnection.getConnection();
+			PreparedStatement preStatement = conn.prepareStatement(sql);
+			result = preStatement.executeQuery();
 		} catch (SQLException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return conn;
+		return result;
 	}
 }
