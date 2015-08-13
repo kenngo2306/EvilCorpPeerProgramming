096c11549dac6e0dc647bd94b0d8878542c82e90
KenNgo <phucuongngo@gmail.com>
2015-08-13 10:00:04 -0400

    initialize connection classes

--- EvilCorpPeerProgramming/src/main/AccountDBHelper.java
+++ EvilCorpPeerProgramming/src/main/AccountDBHelper.java
@@ -1,6 +1,31 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class AccountDBHelper
 {
-
+	public ArrayList<Account> getAllAccounts()
+	{
+		ArrayList<Account> accounts = new ArrayList<Account>();
+		
+		return accounts;
+	}
+	
+	private Connection getConnection()
+	{
+		Connection conn = null;
+		try
+		{
+			conn =  DBConnection.getConnection();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
--- EvilCorpPeerProgramming/src/main/TransactionDBHelper.java
+++ EvilCorpPeerProgramming/src/main/TransactionDBHelper.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
 public class TransactionDBHelper
 {
-
+	
 }
