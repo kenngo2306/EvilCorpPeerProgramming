84eaab0acffbc8e94e9c734a4c5ec106d3b1edaf
ahmed <ahha5402@colorado.edu>
2015-08-13 10:23:51 -0400

    Account submitted

--- EvilCorpPeerProgramming/src/main/Account.java
+++ EvilCorpPeerProgramming/src/main/Account.java
@@ -1,6 +1,46 @@
 package main;
 
+import java.util.GregorianCalendar;
+
 public class Account
 {
-
+	private int ID;
+	private long account_number;
+	private String name;
+	private int starting_balance;
+	private GregorianCalendar birth_date;
+	
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public long getAccount_number() {
+		return account_number;
+	}
+	public void setAccount_number(long account_number) {
+		this.account_number = account_number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStarting_balance() {
+		return starting_balance;
+	}
+	public void setStarting_balance(int starting_balance) {
+		this.starting_balance = starting_balance;
+	}
+	public GregorianCalendar getBirth_date() {
+		return birth_date;
+	}
+	public void setBirth_date(GregorianCalendar birth_date) {
+		this.birth_date = birth_date;
+	}
+	
+	
 }
